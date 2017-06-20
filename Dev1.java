f69ebe7990b8e0657b4b725621e9e0b5e2233087
alexandramila <alexandramila@mail.ru>
2017-06-20 22:09:31 +0300

    Update Dev1.java

--- Dev1.java
+++ Dev1.java
@@ -1,12 +1,12 @@
 public class Dev1 {
-	public static void main(String[] args) {
-		for (int n = 0; n <= 100; n++) {
-			if (n % 3 == 0) {
-				System.out.println("3*" + n/3);
-			} else {
-				System.out.printl(n);
-			}
-		}
-		return;
+    public static void main(String[] args) {
+	for (int n = 0; n <= 100; n++) {
+	    if (n % 3 == 0) {
+	        System.out.println("3*" + n/3);
+	    } else {
+	        System.out.printl(n);
+	    }
 	}
+	return;
+    }
 }
