b66a1fb73537ef023317422f456fff2e64480def
alexandramila <alexandramila@mail.ru>
2017-06-21 21:45:02 +0300

    Update Dev1.java

--- Dev1.java
+++ Dev1.java
@@ -1,7 +1,7 @@
 public class Dev1 {
   public static void main(String[] args) {
     for (int n = 0; n <= 100; n++) {
-      if ((n % 3 == 0) && (n % 5 == 0)) {
+      if (n % 3 == 0 && n % 5 == 0) {
 	System.out.println("Tutti-Frutti");
       } else if (n % 3 == 0) {
 	System.out.println("Tutti");
