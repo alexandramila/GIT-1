a078d42db78d9830d8bf07c8c9bb53a3e6149771
alexandramila <alexandramila@mail.ru>
2017-06-24 15:20:39 +0300

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -7,15 +7,20 @@ public class Fibonacci {
     double sqrt5 = Math.sqrt(5);
     double GoldenRatio = (1 + sqrt5) / 2;
     Scanner input = new Scanner(System.in);
-    System.out.println("Enter a positive number: ");
+    System.out.println("Enter a positive number to 2147483647: ");
     int number = input.nextInt();
-    while (FibNumber < number) {
-      FibNumber = Math.round(Math.pow(GoldenRatio, i) / sqrt5);
-      i++;
-    } if (number == FibNumber) {
-      System.out.println("Your number is the Fibonacci number.");
+    if (number > 0) {
+      while (FibNumber < number) {
+        FibNumber = Math.round(Math.pow(GoldenRatio, i) / sqrt5);
+        i++;
+      } if (number == FibNumber) {
+        System.out.println("Your number is the Fibonacci number.");
+      } else {
+        System.out.println("Your number isn't the Fibonacci number.");
+      }
     } else {
-      System.out.println("Your number isn't the Fibonacci number.");
-    }
+      System.out.println("Your number is negative.");
+      System.exit(0);
+    }   
   }
 }
