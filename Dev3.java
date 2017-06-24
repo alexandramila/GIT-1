bc18de1d71cd2dfee610fed17ae49c0f518ca72d
alexandramila <alexandramila@mail.ru>
2017-06-24 14:53:21 +0300

    Add files via upload
    The Fibonacci numbers.

---
+++ Dev3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+  public static void main(String[] args) {
+    int i = 0;
+    double FibNumber = 0;
+    double sqrt5 = Math.sqrt(5);
+    double GoldenRatio = (1 + sqrt5) / 2;
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter a positive number: ");
+    int number = input.nextInt();
+    while (FibNumber < number) {
+      FibNumber = Math.round(Math.pow(GoldenRatio, i) / sqrt5);
+      i++;
+    } if (number == FibNumber) {
+      System.out.println("Your number is the Fibonacci number.");
+    } else {
+      System.out.println("Your number isn't the Fibonacci number.");
+    }
+  }
+}
