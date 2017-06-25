8babd8589a92c38cf0e505121b99c7ffe1c6f7b9
alexandramila <alexandramila@mail.ru>
2017-06-25 17:06:23 +0300

    Add files via upload

---
+++ Sequence.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Sequence {
+  public static void main(String[] args) {
+    boolean test = true; 
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter numerical sequence /use space while entering/:");
+    String s = input.nextLine();
+    String gaps[] = s.split(" "); 	
+    int seq[] = new int[gaps.length];  
+    for (int i = 1; i < seq.length; i++) {
+      if (seq[i] >= seq[i-1]) {
+        test = true;
+      } else {
+        System.out.println("Your numerical sequence is decreasing.");
+        test = false;
+        break;
+      }
+    } 
+    if (test == true) {
+      System.out.println("Your numerical sequence is non-decreasing.");
+    }		
+  }
+}
