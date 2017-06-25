00015cfc1159826101f06d4f330e407d20ef8abd
alexandramila <alexandramila@mail.ru>
2017-06-25 18:21:19 +0300

    Update Sequence.java

--- Sequence.java
+++ Sequence.java
@@ -5,9 +5,17 @@ public class Sequence {
     boolean test = true; 
     Scanner input = new Scanner(System.in);
     System.out.println("Enter numerical sequence /please use space while entering/:");
-    String s = input.nextLine();
+    с
     String gaps[] = s.split(" "); 	
     int seq[] = new int[gaps.length];  
+    try {
+      for (int i = 0; i < gaps.length; i++) {
+        seq[i] = Integer.parseInt(gaps[i]);
+      }
+    } catch (Exception e) {
+        System.out.println("You've entered invalid number format.");
+        System.exit(0);
+      }
     for (int i = 1; i < seq.length; i++) {
       if (seq[i] >= seq[i-1]) {
         test = true;
