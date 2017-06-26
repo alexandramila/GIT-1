91a0856aa5d61a5f8fdd8b48be2e76d4e64174a8
alexandramila <alexandramila@mail.ru>
2017-06-27 00:07:01 +0300

    Update Sequence.java

--- Sequence.java
+++ Sequence.java
@@ -2,31 +2,25 @@ import java.util.Scanner;
 
 public class Sequence {
   public static void main(String[] args) {
-    boolean test = true; 
-    Scanner input = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
     System.out.println("Enter numerical sequence /please use space while entering/:");
+    String s = scanner.nextLine();
     String gaps[] = s.split(" "); 	
-    int seq[] = new int[gaps.length];  
+    int sequence[] = new int[gaps.length];  
     try {
       for (int i = 0; i < gaps.length; i++) {
-        seq[i] = Integer.parseInt(gaps[i]);
+        sequence[i] = Integer.parseInt(gaps[i]);
       }
     } catch (Exception e) {
         System.out.println("You've entered invalid number format.");
-        System.exit(0);
+        return;
       }
-    for (int i = 1; i < seq.length; i++) {
-      if (seq[i] >= seq[i-1]) {
-        test = true;
-      } else {
-        test = false;
-        break;
+    for (int i = 1; i < sequence.length; i++) {
+      if (sequence[i] < sequence[i-1]) {
+        System.out.println("Your numerical sequence is not non-decreasing.");
+        return;
       }
     } 
-    if (test == true) {
-      System.out.println("Your numerical sequence is non-decreasing.");
-    } else {
-      System.out.println("Your numerical sequence is decreasing.");
-    }
+    System.out.println("Your numerical sequence is non-decreasing.");
   }
 }
