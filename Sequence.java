fd7ba9da8a764ce2b3008ab70d12f913f700d4cc
Alexandra Milashevskaya <alexandramila@mail.ru>
2017-06-28 14:54:26 +0300

    Update Sequence.java

--- Sequence.java
+++ Sequence.java
@@ -2,10 +2,18 @@ import java.util.Scanner;
 
 public class Sequence {
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Enter numerical sequence /please use space while entering/:");
-    String s = scanner.nextLine();
-    String gaps[] = s.split(" "); 	 
+    if (args.length != 0) {
+      checkNondecreasing(args);
+    } else {
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("Enter numerical sequence /please use space while entering/:");
+      String s = scanner.nextLine();
+      String gaps[] = s.split(" ");
+      checkNondecreasing(gaps);
+    }
+  }
+  
+  public static void checkNondecreasing(String[] gaps) {
     try {
       for (int i = 1; i < gaps.length; i++) {
         if (Integer.parseInt(gaps[i]) < Integer.parseInt(gaps[i-1])) {
