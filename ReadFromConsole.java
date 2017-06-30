3e76081015a2f3107cf840f9347601cf01d5cf7b
Alexandra Milashevskaya <alexandramila@mail.ru>
2017-06-30 16:07:30 +0300

    Update ReadFromConsole.java

--- ReadFromConsole.java
+++ ReadFromConsole.java
@@ -1,11 +1,14 @@
 /**
  * Reads data entered by a user from a console.
+ */
+class ReadFromConsole {
+ /**
+ * Checks if data has been entered via the console.
  * @param args   the data that was entered by the user
  *               in the console
  * @return true  when the data has been entered
  * @return false when the user didn't entered the data in the console  
  */
-class ReadFromConsole {
   public static boolean read(String[] args) {
     if (args.length != 0) {
       return true;
