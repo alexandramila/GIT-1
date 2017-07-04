eea71688beb82ee658a8e4506adf3dfb4c87bd88
Alexandra Milashevskaya <alexandramila@mail.ru>
2017-07-04 15:05:09 +0300

    Update CheckSequence.java

--- CheckSequence.java
+++ CheckSequence.java
@@ -2,12 +2,12 @@
  * Analyzes the sequence that was entered by the user.
  */
 class CheckSequence {
-/**
- * Determines whether the sequence is non-decreasing,
- * and displays the result on the screen.
- * @param gaps the sequence that was entered by the user
- *             and that must be checked.
- */
+  /**
+   * Determines whether the sequence is non-decreasing,
+   * and displays the result on the screen.
+   * @param gaps the sequence that was entered by the user
+   *             and that must be checked.
+   */
   public void checkNondecreasing(String[] gaps) {
     try {
       for (int i = 1; i < gaps.length; i++) {
@@ -19,7 +19,6 @@ class CheckSequence {
       System.out.println("Your numerical sequence is non-decreasing.");
     } catch (Exception e) {
       System.out.println("You've entered invalid number format.");
-      return;
     }
   }
 }
