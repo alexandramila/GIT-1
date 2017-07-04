5fb04e0f9632a6afc7a621b88bd3a493cbfcbb0e
Alexandra Milashevskaya <alexandramila@mail.ru>
2017-07-04 18:45:05 +0300

    Update and rename ReadFromCommandLine.java to CommandLineArguments.java

--- CommandLineArguments.java
+++ CommandLineArguments.java
@@ -1,14 +1,14 @@
 /**
  * Reads data entered by a user from a command line.
  */
-class ReadFromCommandLine {
+class CommandLineArguments {
   /**
    * Checks if data has been entered via the command line.
    * @param args   the data that was entered by the user
    *               in the command line
    * @return the sequance  
    */
-  public String[] readSequenceCommandLine(String[] args) {
+  public String[] sequenceCommandLine(String[] args) {
     if (args.length != 0) {
       return args;
     } else {
