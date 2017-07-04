632399fcc0d0561655d63e8f5232f0716698e7d6
Alexandra Milashevskaya <alexandramila@mail.ru>
2017-07-04 18:11:06 +0300

    Update Sequence.java

--- Sequence.java
+++ Sequence.java
@@ -1,17 +1,16 @@
-import java.util.Scanner;
-
 public class Sequence {
   public static void main(String[] args) {
-    ReadFromConsole read = new ReadFromConsole();
+    ReadFromCommandLine read = new ReadFromCommandLine();
     CheckSequence check = new CheckSequence();
-    if (read.readSequence(args)) {
-      check.checkNondecreasing(args);
-    } else {
-      Scanner scanner = new Scanner(System.in);
-      System.out.println("Enter numerical sequence /please use space while entering/:");
-      String s = scanner.nextLine();
-      String gaps[] = s.split(" ");
-      check.checkNondecreasing(gaps);
+    try {
+      if (check.checkNondecreasing(read.readSequenceCommandLine(args))) {
+        System.out.println("Your numerical sequence is non-decreasing.");
+      } else {
+        System.out.println("Your numerical sequence is not non-decreasing.");
+      }
+      return;
+    } catch (Exception e) {
+      System.out.println("You've entered invalid number format.");
     }
   }
 }
