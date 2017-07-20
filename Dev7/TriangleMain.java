5f77643b63be0bc1912b10d1e955b44633291430
Alexandra Milashevskaya <alexandramila@mail.ru>
2017-07-20 21:49:48 +0300

    Removed try-catch.

--- Dev7/TriangleMain.java
+++ Dev7/TriangleMain.java
@@ -4,15 +4,7 @@ public class TriangleMain {
   public static void main(String[] args) {
     InputSides input = new InputSides();
     double[] array = input.inputSides();
-    try {
-      Triangle triangle = new Triangle(array);
-    } catch (IllegalArgumentException e) {
-      System.out.println(e.getMessage());
-      return;
-    } catch(InputMismatchException e) { 
-      System.out.println("Instead of the number, another character was entered.");
-      return;
-    } 
+    Triangle triangle = new Triangle(array);
     try {
       EquilateralTriangle equilateral = new EquilateralTriangle(array);
       System.out.println("This triangle is equilateral.");
