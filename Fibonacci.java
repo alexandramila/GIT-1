d80c94fa34db9b464a601e4f6de87125fa809c06
Alexandra Milashevskaya <alexandramila@mail.ru>
2018-01-08 23:54:02 +0300

    update

--- Fibonacci.java
+++ Fibonacci.java
@@ -3,23 +3,24 @@ import java.util.Scanner;
 public class Fibonacci {
   public static void main(String[] args) {
     int i = 0;
-    double FibNumber = 0;
+    double fibNumber = 0;
     final double sqrt5 = Math.sqrt(5);
-    double GoldenRatio = (1 + sqrt5) / 2;
-    Scanner input = new Scanner(System.in);
+    double goldenRatio = (1 + sqrt5) / 2;
+    Scanner scanner = new Scanner(System.in);
     System.out.println("Enter a positive number to 2147483647: ");
-    int number = input.nextInt();
+    int number = scanner.nextInt();
     if (number >= 0) {
-      while (FibNumber < number) {
-        FibNumber = Math.round(Math.pow(GoldenRatio, i) / sqrt5);
+      while (fibNumber < number) {
+        fibNumber = Math.round(Math.pow(goldenRatio, i) / sqrt5);
         i++;
-      } if (number == FibNumber) {
+      }
+      if (number == fibNumber) {
         System.out.println("Your number is the Fibonacci number.");
       } else {
         System.out.println("Your number isn't the Fibonacci number.");
       }
     } else {
       System.out.println("Your number is negative.");
-    }   
+    }
   }
 }
--- Sequence.java
+++ Sequence.java
@@ -12,17 +12,18 @@ public class Sequence {
       checkNondecreasing(gaps);
     }
   }
-  
-/**
- * Determines whether the sequence is non-decreasing,
- * and displays the result on the screen.
- * @param gaps the sequence that was entered by a user
- *             and that must be checked.
- */
+
+  /**
+   * Determines whether the sequence is non-decreasing,
+   * and displays the result on the screen.
+   *
+   * @param gaps the sequence that was entered by a user
+   *             and that must be checked.
+   */
   public static void checkNondecreasing(String[] gaps) {
     try {
       for (int i = 1; i < gaps.length; i++) {
-        if (Integer.parseInt(gaps[i]) < Integer.parseInt(gaps[i-1])) {
+        if (Integer.parseInt(gaps[i]) < Integer.parseInt(gaps[i - 1])) {
           System.out.println("Your numerical sequence is not non-decreasing.");
           return;
         }
