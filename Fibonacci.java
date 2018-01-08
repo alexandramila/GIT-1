7320a9fd709ea1353f26fab9f33a8c08fba770d2
Alexandra Milashevskaya <alexandramila@mail.ru>
2018-01-09 00:30:53 +0300

    update

--- Fibonacci.java
+++ Fibonacci.java
@@ -3,24 +3,23 @@ import java.util.Scanner;
 public class Fibonacci {
   public static void main(String[] args) {
     int i = 0;
-    double fibNumber = 0;
+    double FibNumber = 0;
     final double sqrt5 = Math.sqrt(5);
-    double goldenRatio = (1 + sqrt5) / 2;
-    Scanner scanner = new Scanner(System.in);
+    double GoldenRatio = (1 + sqrt5) / 2;
+    Scanner input = new Scanner(System.in);
     System.out.println("Enter a positive number to 2147483647: ");
-    int number = scanner.nextInt();
+    int number = input.nextInt();
     if (number >= 0) {
-      while (fibNumber < number) {
-        fibNumber = Math.round(Math.pow(goldenRatio, i) / sqrt5);
+      while (FibNumber < number) {
+        FibNumber = Math.round(Math.pow(GoldenRatio, i) / sqrt5);
         i++;
-      }
-      if (number == fibNumber) {
+      } if (number == FibNumber) {
         System.out.println("Your number is the Fibonacci number.");
       } else {
         System.out.println("Your number isn't the Fibonacci number.");
       }
     } else {
       System.out.println("Your number is negative.");
-    }
+    }   
   }
 }
